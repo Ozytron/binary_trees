37c21f91abcbc4b9417f5a5cf9edac2ebfef1d5e
Emmanuel Ozioko <emmanuelozioko35@gmail.com>
2023-03-31 09:03:43 -0700

    add correction

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -10,10 +10,14 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	size_t leaves = 0;
 
-	if (tree)
+	if (tree != NULL)
 	{
 		leaves += (!tree->left && !tree->right) ? 1 : 0;
 		leaves += binary_tree_leaves(tree->left);
 		leaves += binary_tree_leaves(tree->right);
+		return (leaves);
 	}
-	return (leaves);
+	else
+	{
+		return (0);
+	}
